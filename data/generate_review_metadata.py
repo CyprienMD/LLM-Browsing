switched to matrix-based collective jaccard computation & corrected evaluation script
Signed-off-by: micheldc <micheldc@lig-gpu1.imag.fr>

data/generate_review_metadata.py
@@ -21,6 +21,7 @@ from spacy.lang.en import English
 from textblob import TextBlob
 import pandas as pd
 from tqdm import tqdm
+import time
 nltk.download('wordnet')
 nltk.download('stopwords')
 
@@ -235,7 +236,8 @@ def index_thread_function(elements, threadNumber, thread_boundaries, text_vector
         end_index = thread_boundaries[threadNumber]
         print(f"started n{threadNumber} {start_index} {end_index}")
         for index1, element1 in tqdm(elements.iloc[start_index:end_index].iterrows(), total=(end_index-start_index)):
-            for index2, element2 in elements.loc[index1+1:(len(elements)-1)].iterrows():
+            #for index2, element2 in elements.loc[index1+1:(len(elements)-1)].iterrows(): # * previous
+            for index2, element2 in elements.loc[index1+1:end_index].iterrows(): # ! modified
                 if element1.type == 'item' or element2.type == 'item' or element1.item_id == element2.item_id:
 
                     SIMILARITY = LLMSimilarity
@@ -245,12 +247,6 @@ def index_thread_function(elements, threadNumber, thread_boundaries, text_vector
                     if math.isnan(text_similarity) == True:
                         text_similarity = 0
 
-                    try:
-                        assert text_similarity == 100.
-                    except:
-                        print(text_vectors[index1], text_vectors[index2])
-                        print("text_similarity:", text_similarity)
-                        raise(Exception("unexpected embeddings"))
 
                     summary_similarity = SIMILARITY.cosine_word_embedding(
                         summary_vectors[index1], summary_vectors[index2])
@@ -312,11 +308,14 @@ class Similarity:
     # ... reviews or their their summaries.
     def generate_summary_review_relevance(self):
         texts = self.elements["text"].to_list()
+        print("Vectorizing texts:")
         text_vectors = self.vectorize(texts)
         summaries = self.elements["summary"].to_list()
+        print("Vectorizing summaries:")
         summary_vectors = self.vectorize(summaries)
         tags = self.elements["tags"].to_list()
         tags = list(map(";".join, tags))
+        print("Vectorizing tags:")
         tag_vectors = self.vectorize(tags)
         print("Computing similarities")
         thread_boundaries = self.get_thread_boundaries(
@@ -410,6 +409,7 @@ class GloVESimilarity(Similarity):
         vectors = []
         count_failure = 0
         print("Vectorizing texts")
+
         for text in tqdm(texts):
             # try:
             embeddings = [self.language_model.get(
@@ -437,13 +437,40 @@ class LLMSimilarity(Similarity):
 
         self.language_model_name = language_model_name
         if self.language_model_name == "voyage":
-            self.language_model_client = voyageai.Client()
+            with open("voyage_API_key.txt", "r") as f:
+                api_key = f.read()
+            self.language_model_client = voyageai.Client(api_key=api_key)
         else:
             raise Exception("language model not recognized")
         super().__init__(db_interface, process_count=process_count)
     
     # This function returns numerical embeddings for the textual content of reviews and their summaries.
     def vectorize(self, texts):
-        #return [np.zeros(5) for i in range(len(texts))] # ! TODO: remove
-        return self.language_model_client.embed(texts, model="voyage-large-2-instruct")
-    
+        tokenized = self.language_model_client.tokenize(texts)
+
+        # print(f"request API for {len(texts)} texts, totalling {self.language_model_client.count_tokens(texts)} tokens")
+        # return [np.zeros(5) for i in range(len(texts))]
+        #print("len(texts): ", len(texts), len(texts[0]))
+        #raise()
+        def batch_requests(texts, max_tpm, max_batch_size):
+            tokenized = self.language_model_client.tokenize(texts)
+            batch = []
+            current_batch_tokens = 0
+            for i,text in enumerate(texts):
+                nb_tokens = len(tokenized[i])
+
+                if current_batch_tokens + nb_tokens < max_tpm and len(batch) + 1 < max_batch_size:
+                    batch.append(text)
+                    current_batch_tokens += nb_tokens
+                else:
+                    yield batch
+                    batch = [text]
+                    current_batch_tokens = nb_tokens
+            yield batch          
+        batched_requests = batch_requests(texts, 8000, 128) # for a theoretical limit of 10k tpm
+        res = []
+        for batch in batched_requests:
+            res += [np.asarray(l) for l in self.language_model_client.embed(batch, model="voyage-large-2-instruct").embeddings]
+            time.sleep(65)
+            #print("size of embedding: ", [r.shape for r in res])
+        return res

evaluate_deep_agent.py
@@ -42,7 +42,7 @@ for arg in args:
 torch.set_num_threads(5)
 
 # Define an instance of the INTEX environment
-env: intex_env = gym.make('intex-env-v1')
+env: intex_env = gym.make('intex-env-v1', disable_env_checker=True)
 with DBInterface(dataset) as db_interface:
 
     target_query = intex_experiments.target_query(test=True)
@@ -92,15 +92,13 @@ with DBInterface(dataset) as db_interface:
     if algorithm == "DQN":
 
         q_function = q_function(observation_size, env.get_action_space_size())
-        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        print("working on device:", "cuda" if torch.cuda.is_available() else "cpu")
-        q_function.to(device)
         replay_buffer = pfrl.replay_buffers.ReplayBuffer(capacity=10 ** 5)
         # Use Adam optimizer to optimize the Q function. We set eps=1e-2 for stability.
         optimizer = torch.optim.Adam(q_function.parameters(
         ), lr=configuration.learning_configurations["alpha"], eps=1e-2)
         agent = pfrl.agents.DoubleDQN(q_function, optimizer, replay_buffer, gamma, explorer,
-                                      replay_start_size=50, update_interval=1, target_update_interval=100, phi=phi, gpu=-1, recurrent=False)
+                                      replay_start_size=50, update_interval=1, target_update_interval=100, phi=phi, gpu=0, recurrent=False)
+        print("working on device:", agent.device)
         network_width = q_function.network_width
     elif algorithm == "DQN Recurrent":
         network_width = 1024
@@ -152,7 +150,8 @@ with DBInterface(dataset) as db_interface:
                                 num_processes=1, phi=phi, max_grad_norm=None)
 
     # WandB init and config
-    wandb.init(project='ddqn', entity='intex')
+    name = f"{dataset}_{target_variant}_{operator_variant}_{transfer_variant}_{reward_variant}"
+    wandb.init(project='llm&databases', entity='cyprienm', name=name+"_evaluation")
     config = wandb.config
     config.dataset = db_interface.dataset
     config.algorithm = algorithm
@@ -193,8 +192,7 @@ with DBInterface(dataset) as db_interface:
         observation_size), columns=feature_activation_columns)
 
     # load the trained model based on the testing variant
-    agent_model_address = "model/{}_t{}/170000_finish".format(
-        dataset, target_variant[1])
+    agent_model_address = f"model/{name}/best"
     agent.load(agent_model_address)
     # agent.load("dqn-agent")
 

utilities.py
@@ -5,28 +5,35 @@ import numpy as np
 from numpy import exp
 import sys
 import configuration
+from scipy.spatial.distance import pdist, squareform
 
 import datetime # my addition
 
-
+# - collective_jaccard_calls = 0
 
 # ***** PART 1: INTERNAL HELPER FUNCTIONS FOR QUALITY FUNCTIONS *****
 
 # In case quality_function is "diverse_review", we compute the diversity of reviews
 # ... using collective Jaccard.
-def collective_jaccard(text):
+def collective_jaccard(text): # deprecated
+
+    raise(Exception("collective_jaccard methpd is deprecated, use collective_jaccard_diversity"))
 
     words = []
     quality_score = 0
+
+    global collective_jaccard_calls
+    collective_jaccard_calls += 1
 
     # Add all words in the reviews to the list "words"
     # Each review has its own set of words.
     # Note that obviously the words could be repetitive.
     for review in text:
         review_words = review.split(" ")
+        # - review_words = list(set(review_words)) # ! was not in original code
         words.append(review_words)
     
-    print("number of words:", len(words))
+    jaccard_distances = []
 
     # Check a pair of reviews at a time, and compute the intersection and the union.
     # The Jaccard diversity score (between each pair of reviews) is obtained by
@@ -37,8 +44,10 @@ def collective_jaccard(text):
             intersected_words = [word for word in words[i] if word in words[j]]
             union_words = words[i] + words[j]
             pairwise_jaccard = 1 - (len(intersected_words) / len(union_words))
+            jaccard_distances.append(pairwise_jaccard)
             quality_score += pairwise_jaccard
             cnt += 1
+    # - print("pairwise jaccard distances:", jaccard_distances)
 
     # The overall Jaccard score is the average of individual score, i.e, the sum
     # ... divided by the count. For any reason, if the count remained zero, the
@@ -49,9 +58,48 @@ def collective_jaccard(text):
     except:
         return 0
 
+# - def get_collective_jaccard_calls():
+# -     return collective_jaccard_calls
+
+def lists_to_binary_matrix(lists):
+    # Get the unique elements in all lists
+    unique_elements = sorted(set().union(*lists))
+    element_index = {element: idx for idx, element in enumerate(unique_elements)}
+    
+    # Create a binary matrix where each row corresponds to a list
+    binary_matrix = np.zeros((len(lists), len(unique_elements)), dtype=int)
+    for i, lst in enumerate(lists):
+        for element in lst:
+            binary_matrix[i, element_index[element]] = 1
+            
+    return binary_matrix
+
+def collective_jaccard_diversity(texts):
+
+    # - global collective_jaccard_calls
+    # - collective_jaccard_calls += 1
+
+    words = []
+    for review in texts:
+        review_words = review.split(" ")
+        words.append(review_words)
+
+    binary_matrix = lists_to_binary_matrix(words)
+    
+    # Compute pairwise Jaccard distances
+    jaccard_distances = pdist(binary_matrix, metric='jaccard')
+
+    # - print("jaccard distances:", jaccard_distances)
+    
+    # Compute the average Jaccard diversity
+    average_jaccard_diversity = np.mean(jaccard_distances)
+    
+    return average_jaccard_diversity
+
+
+
 # In case quality_function is "coverage_review", we compute the coverage of reviews
 # ... using the unique word count.
-
 
 def unique_word_count(reviews):
 